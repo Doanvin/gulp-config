d6048a67d3a1489c752f5dba8921631d8e75d0d7
Donavin <donavinhannon@gmail.com>
2017-07-17 01:47:56 -0700

    Added eslint, sass-lint, yarn, taffy scss, js sourcemaps to gulp

--- src/js/main.js
+++
@@ -1,4 +0,0 @@
-console.log("The dog hoped over the fence.");
-console.log("the fence.");
-console.log("Yellow Jello Mellow.");
-document.getElementById('about').innerHTML = 'Marry Poppins is Dope';
